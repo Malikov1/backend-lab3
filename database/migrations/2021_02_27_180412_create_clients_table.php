5ff726d75b73c8a6863ecca8f28266c5def1bbb5
Azamat Malikov <daddy@MacBook-Air-Azamat.local>
2021-02-27 23:15:43 +0500

    clients db created

--- database/migrations/2021_02_27_180412_create_clients_table.php
+++ database/migrations/2021_02_27_180412_create_clients_table.php
@@ -16,7 +16,7 @@ class CreateClientsTable extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('suname');
+            $table->string('surname');
             $table->integer('age');
             $table->timestamps();
         });
